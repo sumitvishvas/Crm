f0e4104fc2f03c256bac24e6f01d384a4c6c71bf
Sumit Vishvas <vishvassumit333@gmail.com>
2023-12-09 12:58:55 +0530

    dev branch acording bar color change

--- application/config/app-config.php
+++ application/config/app-config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 * environments.
 *
 */
-define('APP_BASE_URL', 'http://localhost/apps/crm/');
+define('APP_BASE_URL', 'http://localhost/Crm/');
 
 /*
 * --------------------------------------------------------------------------
